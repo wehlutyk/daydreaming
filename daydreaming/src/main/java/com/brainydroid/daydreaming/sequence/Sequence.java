65e5fc6ce171c9cf7f219a18d1dbfae5a21127f3
Sébastien Lerique <sl@mehho.net>
2014-09-16 13:42:20 +0200

    Refine sequence statuses

--- daydreaming/src/main/java/com/brainydroid/daydreaming/sequence/Sequence.java
+++ daydreaming/src/main/java/com/brainydroid/daydreaming/sequence/Sequence.java
@@ -6,7 +6,6 @@ import com.brainydroid.daydreaming.db.SequenceJsonFactory;
 import com.brainydroid.daydreaming.db.SequencesStorage;
 import com.brainydroid.daydreaming.db.TypedStatusModel;
 import com.brainydroid.daydreaming.db.Views;
-import com.fasterxml.jackson.annotation.JacksonInject;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.google.inject.Inject;
 
@@ -22,14 +21,38 @@ public class Sequence extends TypedStatusModel<Sequence,SequencesStorage,Sequenc
 
     public static String[] AVAILABLE_TYPES = new String[] {TYPE_PROBE, TYPE_BEGIN_QUESTIONNAIRE};
 
+    /* Probes/Questionnaires: notification has appeared */
+    public static final String STATUS_PENDING = "pending";
+    /* Probes: notification was dismissed, and probe not yet re-suggested nor dropped */
+    public static final String STATUS_RECENTLY_DISMISSED = "recentlyDismissed";
+    /* Probes: notification died away, and probe not yet re-suggested nor dropped */
+    public static final String STATUS_RECENTLY_MISSED = "recentlyMissed";
+    /* Probes/Questionnaires: activity is running */
+    public static final String STATUS_RUNNING = "running";
+    /**
+     * Probes/Questionnaires: Activity was stopped in the middle of the sequence,
+     * and probe not yet re-suggested nor dropped
+     */
+    public static final String STATUS_RECENTLY_PARTIALLY_COMPLETED = "recentlyPartiallyCompleted";
+    /**
+     * Probes: was re-suggested after miss/dismiss/partialCompletion, and was refused.
+     * Or, dropped after too long missed/dismissed/partialCompletion.
+     */
+    public static final String STATUS_MISSED_OR_DISMISSED_OR_INCOMPLETE = "missedOrDismissedOrIncomplete";
+    /* Probes/Questionnaires: sequence completely answered */
+    public static final String STATUS_COMPLETED = "completed";
+    /* Questionnaires: sequence completely answered, uploaded, and must be kept locally */
     public static final String STATUS_UPLOADED_AND_KEEP = "uploadedAndKeep";
-    public static final String STATUS_PENDING = "pending"; // Notification has appeared
-    public static final String STATUS_RUNNING = "running"; // Activity is running
-    public static final String STATUS_PARTIALLY_COMPLETED = "partiallyCompleted"; // Activity was stopped (if a probe, it expired, if a questionnaire, can be resumed)
-    public static final String STATUS_COMPLETED = "completed"; // Activity completed
-    public static final String STATUS_MISSED_OR_INCOMPLETE = "missed"; // For probes: Notification died away
 
-    public static String[] AVAILABLE_STATUSES = new String[] {STATUS_PENDING,STATUS_RUNNING,STATUS_PARTIALLY_COMPLETED,STATUS_COMPLETED};
+    public static String[] AVAILABLE_STATUSES = new String[] {
+            STATUS_PENDING,
+            STATUS_RECENTLY_DISMISSED,
+            STATUS_RECENTLY_MISSED,
+            STATUS_RUNNING,
+            STATUS_RECENTLY_PARTIALLY_COMPLETED,
+            STATUS_MISSED_OR_DISMISSED_OR_INCOMPLETE,
+            STATUS_COMPLETED,
+            STATUS_UPLOADED_AND_KEEP};
 
     @JsonView(Views.Public.class)
     private String name = null;
